87c1fcc4311daaa23648c72902ef8bc632130c4f
Peter Bohm <peter@nextlogic.biz>
2022-08-10 13:22:10 +1000

    Added UCS

--- src/grid_node.py
+++ src/grid_node.py
@@ -5,3 +5,13 @@ class GridNode:
     def __init__(self, state: Tuple[int, int], actions: Tuple):
         self.state = state
         self.actions = actions
+
+
+class GridNodeWithCost(GridNode):
+    def __init__(self, state: Tuple[int, int], actions: Tuple, cost: int):
+        super().__init__(state, actions)
+
+        self.cost = cost
+
+    def __lt__(self, other):
+        return self.cost < other.cost
---
+++ src/search/ucs.py
@@ -0,0 +1,34 @@
+import heapq
+import time
+from typing import Tuple, List, Dict
+
+from src.grid_node import GridNodeWithCost
+from src.grid_world import GridWorldWithCost
+
+
+def uniform_cost_search(env: GridWorldWithCost, start: Tuple[int, int], goal: Tuple[int, int]) -> (List[int], int):
+    t0 = time.time()
+
+    visited: Dict[Tuple[int, int], int]= {}
+
+    heap = [GridNodeWithCost(start, (), 0)]
+    heapq.heapify(heap)
+
+    nodes_generated = 0
+
+    while heap:
+        node = heapq.heappop(heap)
+
+        if node.state == goal:
+            print(f"Found the goal in {len(node.actions)} steps and {time.time() - t0}s. Visited {len(visited)} nodes and generated {nodes_generated}")
+            return node.actions, node.cost
+
+        for action in env.actions(node.state):
+            new_state, cost = env.step(action, node.state)
+            new_cost = cost + node.cost
+
+            if new_state not in visited.keys() or visited[new_state] > new_cost:
+                visited[new_state] = new_cost
+                heapq.heappush(heap, GridNodeWithCost(new_state, node.actions + (action,), new_cost))
+
+                nodes_generated += 1
