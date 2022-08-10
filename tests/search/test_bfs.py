ed1cc0a5f24378c09b10b63b822e4768a8710437
Peter Bohm <peter@nextlogic.biz>
2022-08-10 15:29:18 +1000

    Step now returns new state as well as the cost - which complicates the BFS and DFS but it's kind of precursor to A*

--- tests/search/test_bfs.py
+++ tests/search/test_bfs.py
@@ -1,33 +1,41 @@
 import unittest
 
-from src.grid_world import GridWorldWithObstacles, RIGHT, UP, ACTIONS
+from src.grid_world import GridWorldWithObstacles, RIGHT, UP, ACTIONS, GridWorldWithCost
 from src.search.bfs import breadth_first_search
 
 
 class TestBfs(unittest.TestCase):
+    def setUp(self) -> None:
+        self.env = GridWorldWithObstacles()
+
     def test_2_steps_right(self):
-        env = GridWorldWithObstacles()
         start = (8, 0)
         goal = (8, 2)
 
-        actions = breadth_first_search(env, start, goal)
+        actions = breadth_first_search(self.env, start, goal)
         self.assertEqual(2, len(actions))
         self.assertEqual((RIGHT, RIGHT), actions)
 
     def test_2_steps_up(self):
-        env = GridWorldWithObstacles()
         start = (8, 0)
         goal = (6, 0)
 
-        actions = breadth_first_search(env, start, goal)
+        actions = breadth_first_search(self.env, start, goal)
         self.assertEqual(2, len(actions))
         self.assertEqual((UP, UP), actions)
 
     def test_31b(self):
-        env = GridWorldWithObstacles()
         start = (8, 0)
         goal = (0, 8)
 
-        actions = breadth_first_search(env, start, goal)
+        actions = breadth_first_search(self.env, start, goal)
         self.assertEqual(16, len(actions))
         print(f"Actions took: {tuple(ACTIONS[action] for action in actions)}")
+
+    def test_32a(self):
+        env = GridWorldWithCost()
+        actions = breadth_first_search(env, (8, 0), (0, 8))
+
+        self.assertEqual(16, len(actions))
+        print(f"Actions took: {tuple(ACTIONS[action] for action in actions)}")
+
