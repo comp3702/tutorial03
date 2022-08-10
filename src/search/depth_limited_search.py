3d0a20a4b10d6b75013b773e22664a8d1ea6d80b
Peter Bohm <peter@nextlogic.biz>
2022-08-10 11:32:36 +1000

    Added the max depth for DFS

--- src/search/depth_limited_search.py
+++ src/search/depth_limited_search.py
@@ -1,15 +1,16 @@
 import time
 from collections import deque
-from typing import Tuple, Set, Deque
+from typing import Tuple, Set, Deque, Dict
 
 from src.grid_node import GridNode
 from src.grid_world import GridWorld
 
 
-def depth_limited_search(env: GridWorld, start: Tuple[int, int], goal: Tuple[int, int], limit: int):
+def depth_limited_search(env: GridWorld, start: Tuple[int, int], goal: Tuple[int, int], max_depth: int):
     t0 = time.time()
 
-    visited: Set[Tuple[int, int]] = set()
+    # to allow revisiting of nodes found with fewer steps
+    visited: Dict[Tuple[int, int], int] = {}
 
     stack: Deque[GridNode] = deque()
     stack.append(GridNode(start, ()))
@@ -24,10 +25,9 @@ def depth_limited_search(env: GridWorld, start: Tuple[int, int], goal: Tuple[int
 
         for action in env.actions(node.state):
             new_state = env.step(action, node.state)
-            if new_state not in visited:
-                visited.add(new_state)
+            if (new_state not in visited or visited[new_state] > len(node.actions) + 1) and len(node.actions) + 1 < max_depth:
+                visited[new_state] = len(node.actions) + 1
                 stack.append(GridNode(new_state, node.actions + (action,)))
                 nodes_generated += 1
 
-    print(f"Solution not found in {time.time() - t0}s. Visited {len(visited)} nodes and generated {nodes_generated}")
-    return ()
+    return None
--- tests/search/test_depth_limited_search.py
+++ tests/search/test_depth_limited_search.py
@@ -15,3 +15,14 @@ class TestDepthLimitedSearch(unittest.TestCase):
         actions = depth_limited_search(env, start, goal, limit)
         self.assertEqual(2, len(actions))
         self.assertEqual((UP, UP), actions)
+
+    def test_step_up_over_the_limit(self):
+        env = GridWorld()
+        start = (8, 0)
+        goal = (2, 0)
+
+        limit = 4
+
+        actions = depth_limited_search(env, start, goal, limit)
+        # None is the depth limit hit marker!
+        self.assertEqual(None, actions)
